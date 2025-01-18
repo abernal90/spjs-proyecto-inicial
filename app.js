12319ebb0da3c79ecb688287d3e47066948f93b6
Ariel Bernal Aguilar <31418174+abernal90@users.noreply.github.com>
2025-01-18 10:23:40 -0600

    Update app.js
    Solución al ejercicio clase 1 sección 9

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-let numeroSecreto = 6;
+/*Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!".
+Declara una variable llamada nombre y asígnale el valor "Lua".
+Crea una variable llamada edady asígnale el valor 25.
+Establece una variable numeroDeVentas y asígnale el valor 50.
+Establece una variable saldoDisponible y asígnale el valor 1000.
+Muestra una alerta con el texto "¡Error! Completa todos los campos".
+Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos". Ahora muestra una alerta con el valor de la variable mensajeDeError .
+Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre.
+Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
+Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".Si necesitas ayuda, las soluciones a las actividades estarán disponibles en la sección
+*/
+
+alert("¡Bienvenida y bienvenido a nuestro sitio web!");
+let nombre = "Lua";
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+let mensajeDeError = "¡Error! Completa todos los campos";
+alert(mensajeDeError);
+nombre = prompt("Por favor ingrese su nombre:",'');
+edad = prompt("Por favor ingrese su edad:",'');
+if(edad >= 18){
+    alert("¡Puedes obtener tu licencia de conducir!")
+}
+
+/*let numeroSecreto = 6;
 let numeroUsuario = prompt("Me indicas un numero por favor:");
 
 console.log(numeroUsuario);
 
 if (numeroUsuario == numeroSecreto) {
     alert('Acertaste el numero');
-}
+} */
