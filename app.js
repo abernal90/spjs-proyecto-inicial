815a2fa3969431a6d26832795864f6fcadbf1416
Ariel Bernal Aguilar <31418174+abernal90@users.noreply.github.com>
2025-01-18 09:39:39 -0600

    Update app.js
    Se versiona el código de la segunda clase

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-alert('Hola mundo');
-
-//let numeroSecreto = 6;
+let numeroSecreto = 6;
 let numeroUsuario = prompt("Me indicas un numero por favor:");
 
-/*console.log(numeroUsuario);
+console.log(numeroUsuario);
 
 if (numeroUsuario == numeroSecreto) {
     alert('Acertaste el numero');
