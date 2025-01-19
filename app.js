3a1437030f9003fe6467e08d0a7e6f9a357ec3d5
Ariel Bernal Aguilar <31418174+abernal90@users.noreply.github.com>
2025-01-19 11:05:31 -0600

    Modificación clase 3, actividad 4 Operador Ternario

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let numeroSecreto = 8;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraVeces = 'vez';
+
+let maximoIntentos = 2;
 while(numeroUsuario != numeroSecreto){
     
     numeroUsuario = parseInt(prompt("Me indicas un numero entre 1 y 10 por favor:"));
@@ -14,7 +15,7 @@ while(numeroUsuario != numeroSecreto){
 
     if (numeroUsuario == numeroSecreto) {
         // la condición se cumple
-        alert(`Acertaste, el numero es: ${numeroUsuario}, Lo isiste en ${intentos} ${palabraVeces}`);
+        alert(`Acertaste, el numero es: ${numeroUsuario}, Lo isiste en ${intentos} ${intentos==1?'vez':'veces' }`);
     } else{
         // la condición no se cumple
         
@@ -25,6 +26,11 @@ while(numeroUsuario != numeroSecreto){
         }
         intentos++;
         palabraVeces = 'veces';
+
+        if(intentos > maximoIntentos){
+            alert(`Llegaste al número máximo de ${maximoIntentos} intentos`);
+            break;
+        }
     }
 
 }
